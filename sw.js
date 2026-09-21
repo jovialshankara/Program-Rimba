1cf16de29099982dc0345f20ca1549ae12387d87
jovialshankara <jovialshankara@gmail.com>
2026-09-21 12:42:34 +0700

    Add files via upload
    Fix Program Rimba PWA v1.0.1

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='rimba-pwa-v1';const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='rimba-pwa-v2';
+const ASSETS=['./','./index.html','./app.js?v=2','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./'))))});
